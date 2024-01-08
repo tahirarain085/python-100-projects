d54e5341e93c132833021f3a9daebd08eddffe5a
tahirarin085 <tahirarin085@gmail.com>
2024-01-08 23:20:34 +0500

    email sending app in python

---
+++ sendemil.py
@@ -0,0 +1,15 @@
+import smtplib as st 
+
+obj = st.SMTP("smtp.gmail.com",534)
+obj.ehlo()
+obj.starttls()
+obj.login("tahirarain085@gmail.com","@tahirarain")
+subject = "test email "
+body = "I am learning python from various platforms to excell on this language "
+meassage = f"Subject: {subject}\n\n{body}"
+
+listaddress = ["tahirarain085@gmail.com","tahirarainwp@gmail.com"]
+
+sendemail = obj.sendmail("tahirarainwp@gmail.com",listaddress,meassage)
+print("sent email successfully! ")
+obj.quit()
