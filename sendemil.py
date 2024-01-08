fd0d638911ec1d2083e9bca806cfe2d64e00118d
Tahir Munir Faraz <63514187+tahirarain085@users.noreply.github.com>
2024-01-08 23:19:04 +0500

    Update sendemil.py

--- sendemil.py
+++ sendemil.py
@@ -3,7 +3,7 @@ import smtplib as st
 obj = st.SMTP("smtp.gmail.com",534)
 obj.ehlo()
 obj.starttls()
-obj.login("tahirarain085@gmail.com","@tahirarain")
+obj.login("your email","your password")
 subject = "test email "
 body = "I am learning python from various platforms to excell on this language "
 meassage = f"Subject: {subject}\n\n{body}"
